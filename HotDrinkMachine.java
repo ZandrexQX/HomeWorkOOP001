0998470f23119117d8e89e6aa864ae663f6292cf
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-05-27 21:17:37 +0300

    Edit

--- HotDrinkMachine.java
+++ HotDrinkMachine.java
@@ -1,2 +1,32 @@
-public class HotDrinkMachine {
+import java.util.ArrayList;
+
+public class HotDrinkMachine implements VendingMachine {
+    private ArrayList<HotDrink> listDrink;
+
+    public HotDrinkMachine(ArrayList<HotDrink> listDrink){
+        this.listDrink = listDrink;
+    }
+
+    public ArrayList<HotDrink> getListDrink() {
+        return listDrink;
+    }
+
+    public void setListDrink(ArrayList<HotDrink> listDrink) {
+        this.listDrink = listDrink;
+    }
+
+    @Override
+    public ArrayList<HotDrink> getProduct(String name) {
+        return VendingMachine.super.getProduct(name);
+    }
+
+    @Override
+    public ArrayList<HotDrink> getProduct(String name, int volume) {
+        return VendingMachine.super.getProduct(name, volume);
+    }
+
+    @Override
+    public ArrayList<HotDrink> getProduct(String name, int volume, int t) {
+        return VendingMachine.super.getProduct(name, volume, t);
+    }
 }
--- VendingMachine.java
+++ VendingMachine.java
@@ -1,9 +1,32 @@
 import java.util.ArrayList;
 
 public interface VendingMachine {
-    private String name = null;
-    ArrayList<HotDrink>
-    public default void getProduct(int name){
-
+    ArrayList<HotDrink> listDrink = null;
+    default ArrayList<HotDrink> getProduct(String name) {
+        ArrayList<HotDrink> outDrink = new ArrayList<>();
+        for (HotDrink hd: listDrink) {
+            if (hd.getName()==name){
+                outDrink.add(hd);
+            }
+        }
+        return outDrink;
+    }
+    default ArrayList<HotDrink> getProduct(String name, int volume){
+        ArrayList<HotDrink> outDrink = new ArrayList<>();
+        for (HotDrink hd: listDrink) {
+            if (hd.getName()==name && hd.getVolume()==volume){
+                outDrink.add(hd);
+            }
+        }
+        return outDrink;
+    }
+    default ArrayList<HotDrink> getProduct(String name, int volume, int t){
+        ArrayList<HotDrink> outDrink = new ArrayList<>();
+        for (HotDrink hd: listDrink) {
+            if (hd.getName()==name && hd.getVolume()==volume && hd.getT() == t){
+                outDrink.add(hd);
+            }
+        }
+        return outDrink;
     }
 }
