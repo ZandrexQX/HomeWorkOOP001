7510f8c43dc712e441df3c3462fe741579b73151
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-05-30 20:06:40 +0300

    Edit

--- Coffe.java
+++ Coffe.java
@@ -1,5 +1,5 @@
 public class Coffe extends HotDrink{
-    int t = 80;
+    private int t = 80;
     public Coffe(String name, int volume) {
         super(name, volume);
     }
--- Tea.java
+++ Tea.java
@@ -1,5 +1,5 @@
 public class Tea extends HotDrink{
-    int t = 70;
+    private int t = 70;
 
     public Tea(String name, int volume) {
         super(name, volume);
